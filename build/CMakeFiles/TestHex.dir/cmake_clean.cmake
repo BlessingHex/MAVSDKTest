b069812c9b63a89356855d409cd139b6deadf9f7
BlessingHex <bjgallegoosagbakhore1@sheffield.ac.uk>
2022-08-02 16:26:59 +0100

    Manage to implment a locally build mavlink sdk with a sample project

---
+++ build/CMakeFiles/TestHex.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestHex.dir/main.cpp.o"
+  "CMakeFiles/TestHex.dir/main.cpp.o.d"
+  "TestHex"
+  "TestHex.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestHex.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/TestHex.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for TestHex.
+# This may be replaced when dependencies are built.
